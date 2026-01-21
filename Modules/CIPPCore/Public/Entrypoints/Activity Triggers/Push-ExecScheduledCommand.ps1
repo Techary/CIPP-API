79e189ecbc57f08c21d107b3ac30d0010ec0f735
James Tarran <jtarran@techary.com>
2026-01-21 09:15:12 +0000

    Nested option
    more trying to pass ticket ID

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
@@ -329,7 +329,7 @@ function Push-ExecScheduledCommand {
         Write-Information 'Scheduler: Sending the results to the target.'
         Write-Information "The content of results is: $Results"
         switch -wildcard ($task.PostExecution) {
-            '*psa*' { Send-CIPPAlert -Type 'psa' -Title $title -HTMLContent $HTML -TenantFilter $Tenant -PSATicketId $task.Parameters.psaticketid; Write-LogMessage API 'HaloPSATicket' -sev Error -message "Sending ticket ID $($task.parameters.psaticketid) to Send-CIPPAlert" -LogData $($task.parameters.psaticketid) }
+            '*psa*' { Send-CIPPAlert -Type 'psa' -Title $title -HTMLContent $HTML -TenantFilter $Tenant -PSATicketId $task.Parameters.options.psaticketid; Write-LogMessage API 'HaloPSATicket' -sev Error -message "Sending ticket ID $($task.Parameters.options.psaticketid) to Send-CIPPAlert" -LogData $($task.Parameters.options.psaticketid) }
             '*email*' { Send-CIPPAlert -Type 'email' -Title $title -HTMLContent $HTML -TenantFilter $Tenant }
             '*webhook*' {
                 $Webhook = [PSCustomObject]@{
